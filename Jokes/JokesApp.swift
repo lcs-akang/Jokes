55669cc968eb6c5b74a2881aa15537e5e0dbe547
Aidan Kang <akang@lcs.on.ca>
2023-04-14 11:13:17 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Aidan Kang on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
