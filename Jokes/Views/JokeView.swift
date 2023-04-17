0308ad0f22feeb6781ce6228b0448b3ba8c2f8bf
Aidan Kang <akang@lcs.on.ca>
2023-04-17 14:39:15 -0400

    Made it so that a new joke is loaded automatically only when the app is first launched.

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -96,7 +96,9 @@ struct JokeView: View {
             .navigationTitle("Random Jokes")
         }
         .task {
-            currentJoke = await NetworkService.fetch()
+            if currentJoke == nil {
+                currentJoke = await NetworkService.fetch()
+            }
         }
     }
 }
