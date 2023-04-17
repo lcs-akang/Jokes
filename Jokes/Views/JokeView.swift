8e8b08ff12cc61561b26364373e16a9d29fb10d6
Aidan Kang <akang@lcs.on.ca>
2023-04-17 14:35:05 -0400

    Ensured joke can only be saved after showing punchline; made button easier to distinguish from existing button.

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -88,6 +88,9 @@ struct JokeView: View {
                 }, label: {
                     Text("Save for later")
                 })
+                .disabled(punchlineOpacity == 0.0 ? true : false)
+                .tint(.green)
+                .buttonStyle(.borderedProminent)
                 
             }
             .navigationTitle("Random Jokes")
